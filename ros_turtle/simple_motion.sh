0009b8643e63a588a567abcd4a016d65b53b0fe4
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-08-03 22:32:28 +0530

    Update simple_motion.sh

--- ros_turtle/simple_motion.sh
+++ ros_turtle/simple_motion.sh
@@ -7,16 +7,16 @@ $ rosrun turtlesim turtlesim_node
 # Runs the executable turtle
 $ rosrun turtle
 
-# a topic is a bus over which nodes exchange messgages
+# A topic is a bus over which nodes exchange messgages
 # here, rostopic pub => publish on a topic; -r => no stop execution cycle; 1 => 1Hz rate; /turtle1/cmd_vel => topic;
 # geometry_msgs/Twist => message type to use when publishing; -- => option parser; '[5.0, 0.0, 0.0]' '[0.0, 0.0, 3]' => Linear & angular x,y,z values
 $ rostopic pub -r 1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, 3]'
 
-# displays connections between the various nodes and topics
+# Displays connections between the various nodes and topics
 $ rosrun rqt_graph rqt_graph
 
-# displays real-time linear and angular motion (uses matplotlib)
+# Displays real-time linear and angular motion (uses matplotlib)
 $ rosrun rqt_plot rqt_plot
 
-
+# Publishes data on turtle @60Hz
 $ rostopic hz /turtle1/pose
