744c70816f2aca8302400b558cbcbe71a926e93d
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-08-04 23:51:17 +0530

    Update simple_motion.sh

--- ros_turtle/simple_motion.sh
+++ ros_turtle/simple_motion.sh
@@ -4,12 +4,10 @@ $ roscore
 # Runs turtlesim_node from the package turtlesim
 $ rosrun turtlesim turtlesim_node
 
-
+# Runs the executable turtle
 $ rosrun turtle
 
-# A topic is a bus over which nodes exchange messgages
-# here, rostopic pub => publish on a topic; -r => no stop execution cycle; 1 => 1Hz rate; /turtle1/cmd_vel => topic;
-# geometry_msgs/Twist => message type to use when publishing; -- => option parser; '[5.0, 0.0, 0.0]' '[0.0, 0.0, 3]' => Linear & angular x,y,z values
+
 $ rostopic pub -r 1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, 3]'
 
 # Displays connections between the various nodes and topics
