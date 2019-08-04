e9dea750102cac80b8e1f3c0f576979f400bf831
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-08-04 23:52:26 +0530

    Update simple_motion.sh

--- ros_turtle/simple_motion.sh
+++ ros_turtle/simple_motion.sh
@@ -15,8 +15,8 @@ $ rostopic pub -r 1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[
 # Displays connections between the various nodes and topics
 $ rosrun rqt_graph rqt_graph
 
-
+# Displays real-time linear and angular motion (uses matplotlib)
 $ rosrun rqt_plot rqt_plot
 
-# Publishes data on turtle @60Hz
+
 $ rostopic hz /turtle1/pose
